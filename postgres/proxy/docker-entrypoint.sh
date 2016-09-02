500ae56aa99ba3bdaf8d83d2347d9fbbd352985b
Felicio Mununga <felicio.mununga@gmail.com>
2016-09-02 16:51:25 +0200

    Configure proxy server to permit root login

--- postgres/proxy/docker-entrypoint.sh
+++ postgres/proxy/docker-entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/sh -ex
 
-if [ "basename $1" = "sshd"]; then
+if [ "`basename $1`" = "sshd" ]; then
+  
+  # Generate missing host RSA key-pair for use in SSH protocol 2 connections.
+  ssh-keygen -N "" -f /etc/ssh/ssh_host_rsa_key
+
   exec "$@"
+
 fi
 
 exec "$@" 
