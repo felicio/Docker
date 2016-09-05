334842a7fcc7cffd2d589d911314eeadae59df56
Felicio Mununga <felicio.mununga@gmail.com>
2016-09-05 12:16:34 +0200

    Generate all host key-pars

--- postgres/proxy/docker-entrypoint.sh
+++ postgres/proxy/docker-entrypoint.sh
@@ -3,10 +3,13 @@
 if [ "`basename $1`" = "sshd" ]; then
   
   # Generate missing host RSA key-pair for use in SSH protocol 2 connections.
-  ssh-keygen -N "" -f /etc/ssh/ssh_host_rsa_key
+  ssh-keygen -A
 
+  # Check the validity of the configuration file.
+  sshd -T > /var/log/sshd_test.log
+ 
   exec "$@"
 
 fi
 
-exec "$@" 
+exec "$@"
